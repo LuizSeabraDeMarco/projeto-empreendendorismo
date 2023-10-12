29adaf7bac6fd72d0fe0a90e38f94cca6e55880c
LuizSeabraDeMarco <139667471+LuizSeabraDeMarco@users.noreply.github.com>
2023-10-11 22:28:54 -0300

    Novas atualisações

---
+++ map.js
@@ -0,0 +1,40 @@
+const voltar = document.querySelector('#voltar');
+const input = document.querySelector('#busca_div input');
+const sugestoes = document.querySelector('#sugestoes');
+
+voltar.addEventListener('click', function() {
+  history.back();
+});
+
+input.addEventListener('keyup', function() {
+  // Buscar as sugestões com base no texto digitado.
+  const value = input.value.toLowerCase();
+
+  const sugestoesBusca = [
+    "BrownieMaria",
+    "LactoFree"
+  ];
+
+  // Exibir as sugestões.
+  sugestoes.innerHTML = '';
+  for (const sugestao of sugestoesBusca) {
+    if (sugestao.toLowerCase().includes(value)) {
+      sugestoes.innerHTML += `<li><a href="${sugestao}.html"><span id="icon_sugestao"class="material-symbols-outlined">
+      storefront
+      </span>  ${sugestao}</a></li>`;
+    }
+  }
+});
+
+function clickBusca(){
+  console.log('clickBusca() chamado!');
+  var filtro = document.getElementById("input_busca");
+  var filtro_ul = document.getElementById("sugestoes");
+  if (filtro.style.display == "none" || filtro_ul.style.display == "none"){
+      filtro.style.display = "grid";
+      filtro_ul.style.display = "grid";
+  }else{
+      filtro.style.display = "none"
+      filtro_ul.style.display = "none";
+  }
+  }
--- wheather.js
+++
@@ -1,3 +0,0 @@
-function voltarParaPaginaAnterior() {
-    window.history.go(-1); // ou window.history.go(-1);
-}

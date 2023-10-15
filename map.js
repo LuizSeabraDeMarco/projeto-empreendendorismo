2ec10b08c79c37118135a337454b472ff3211d5c
LuizSeabraDeMarco <139667471+LuizSeabraDeMarco@users.noreply.github.com>
2023-10-15 14:12:00 -0300

    adcionando novos exemplos

--- map.js
+++ map.js
@@ -27,11 +27,45 @@ input.addEventListener('keyup', function() {
 });
 
 function clickBusca(){
-  console.log('clickBusca() chamado!');
-  var filtro = document.getElementById("busca_div");
-  if (filtro.style.display = "none"){
-      filtro.style.display = "grid";
+  var filtro = document.getElementById("input_busca");
+  var filtro2 = document.getElementById("sugestoes");
+  console.log('Chamada a função!')
+  if (filtro.style.display == "block" && filtro2.style.display == "block"){
+      console.log('Chama display none!')
+      filtro.style.display = "none";
+      filtro2.style.display = "none";
   }else{
-      filtro.style.display = "none"
+    console.log('Chama display block!')
+    filtro.style.display = "block";
+    filtro2.style.display = "block";
   }
+}
+
+function FullScreen(){
+  console.log('Chamada Executada');
+  var EstadoTela = true;
+  var fecha = document.getElementById("fecha");
+  var desconto = document.getElementById("savings_icon");
+  var tela =  document.getElementById("header");
+  var busca = document.getElementById("buscar_icon");
+  var voltar = document.getElementById("voltar");
+  var telaFull = document.getElementById("fullscreen")
+  tela.style.height = "100vh";
+  fecha.style.display = "none";
+  busca.style.display = "none";
+  desconto.style.display = "none";
+  voltar.style.display = "none";
+  telaFull.style.position = "relative";
+  telaFull.style.top = "0";
+  telaFull.style.opacity = "50%";
+  telaFull.style.backgroundSize = "50%";
+  telaFull.style.backgroundPosition = "center center";
+  if (EstadoTela == true) {
+    telaFull.onclick = function NoFullScreen() {
+      location.reload();
+      //background-size: cover;
+      //background-position: center center;
+    }
   }
+
+}
--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function clickFiltro() {
 
 //Botão (Filtrar Barracas)
 function click_Filtro_Barracas() {
-    console.log('oi 1')
     // Obtém todos os elementos de entrada de rádio com o atributo "name" igual a "r"
     let radioBtns = document.querySelectorAll("input[name='r']");
 
@@ -18,23 +17,22 @@ function click_Filtro_Barracas() {
     let value_filtro = null;
 
     // Itera pelos botões de rádio para encontrar o valor selecionado
-    console.log('oi 2')
     radioBtns.forEach(function(radio) {
         if (radio.checked) {
-            console.log('oi 3')
             value_filtro = radio.value;
         }
     });
 
     // Obtém o elemento de barracas sem açúcar
     let barraca_lactose = document.getElementById("busca_barraca_filtro");
+    let barraca_acucar = document.getElementById("busca_barraca_filtro_sugar");
     var filtro = document.getElementById("filtro");
-    console.log('oi 4')
 
     // Verifica o valor selecionado e exibe ou oculta as barracas com base nele
     if (value_filtro === "2") { // "sem_lactose" representa a opção de "Sem lactose"
         barraca_lactose.style.display = "inline-block";
         filtro.style.display = "none";
+        barraca_acucar.style.display = "none";
         const form = document.querySelector("form");
 
         form.addEventListener("submit", function(event) {
@@ -42,7 +40,18 @@ function click_Filtro_Barracas() {
 
         // Faz alguma coisa com os dados do formulário
         });
-    } else {
+    } else if(value_filtro === "1"){
+        barraca_acucar.style.display = "inline-block";
+        filtro.style.display = "none";
+        barraca_lactose.style.display = "none";
+        const form = document.querySelector("form");
+
+        form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        // Faz alguma coisa com os dados do formulário
+        });
+    }else {
         barraca_lactose.style.display = "none";
         filtro.style.display = "grid";
     }
